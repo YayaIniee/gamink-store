d2cd5a810375ec41ceaf8dd193b4c47c31b59346
YayaIniee <yayainiee@gmail.com>
2022-04-18 00:28:38 +0700

    menambahkan code ke file footer.php

--- footer.php
+++ footer.php
@@ -6,6 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    
+<footer class="mt-auto">
+  <div class="container-fluid pb-4 mt-5 " style="background-color: rgb(252, 189, 17);">
+    <div class="container" style="background-color: rgb(252, 189, 17);">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-12 text-center mt-3">
+          <img src="pict/Game.png" alt="" style="width:90px"class="img-fluid py-3" >
+          <h3>Pilih Game</h3>
+          "Tersedia beberapa game pilihan kami"
+        </div>
+        <div class="col-lg-3 -3 col-md-6 col-12 text-center mt-3">
+          <img src="pict/Pilih.png" alt="" style="width: 90px;" class="img-fluid py-3">
+          <h3>Pilih Nominal</h3>
+          "Pilih nominal topup yang kamu inginkan"
+        </div>
+        <div class="col-lg-3 -3 col-md-6 col-12 text-center mt-3">
+          <img src="pict/pay.png" alt="" style="width: 90px;" class="img-fluid py-3">
+          <h3>Lakukan pembayaran</h3>
+          "Pilih metode pembayaran dan selesaikan pembayaran"
+        </div>
+        <div class="col-lg-3 -3 col-md-6 col-12 text-center mt-3">
+          <img src="pict/Berhasil.png" alt="" style="width: 90px;" class="img-fluid py-3">
+          <h3>Topup Berhasil</h3>
+          "Topuop akan diproses jika pembayaran telah dilakukan dengan benar"
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="pt-3 p-2 bg-black text-white text-center">
+    © 2021 | GaminkStore
+  </div>
+</footer>
 </body>
 </html>
