78f66eb4bbbb81d67a644c4812b15cf929fe92df
YayaIniee <yayainiee@gmail.com>
2022-04-18 00:21:29 +0700

    mengubah dan menambahkan di file index.php

--- index.php
+++ index.php
@@ -26,10 +26,13 @@
           <!-- The slideshow/carousel -->
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img src="img/foto2.jpg" alt="" class="d-block" style="width:100%">
+              <img src="img/foto!.jpg" alt="" class="d-block" style="width:100%">
             </div>
             <div class="carousel-item">
               <img src="img/foto2.jpg" alt="" class="d-block" style="width:100%">
+            </div>
+            <div class="carousel-item">
+              <img src="img/foto3.jpg" alt="" class="d-block" style="width:100%">
             </div>
           </div>
           <!-- Left and right controls/icons -->
