813b8a4455a3b2f26854454ca2992e28adc57701
YayaIniee <yayainiee@gmail.com>
2022-04-17 23:46:28 +0700

    menambahkan navbar code di file header.php

--- header.php
+++ header.php
@@ -11,6 +11,33 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+<nav class="navbar navbar-expand-sm bg-black navbar-dark sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.html">
+      <img src="" alt="Logo" style="width:150px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <li class="nav-item">
+            <a href="index.html" class="nav-link">HOME</i></a>
+          </li>
+          <li class="nav-item">
+            <a href="About Us.html" class="nav-link">ABOUT US</i></a>
+          </li>
+        </li>   
+      </ul>
+      <form class="d-flex">
+        <div class="input-group ">
+          <input type="text" class="form-control" placeholder="search">
+          <button class="btn btn-warning text-black">Search</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</nav>
 </body>
 </html>
