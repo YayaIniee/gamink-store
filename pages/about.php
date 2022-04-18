7b9734784f930c20e608ebe45f73daf3e69a4791
minangcoding <refidoarjunala@gmail.com>
2022-04-19 00:15:12 +0700

    menambahkan file about.php

--- pages/about.php
+++ pages/about.php
@@ -1,2 +1,59 @@
-<?php 
-?>
+<!-- ABOUT SECTION-->
+<div class="container">
+    <div class="span8 offset2">
+        <div class="row main-top-margin text-center">
+            <div class="col-md-8 col-md-offset-2">
+                <h1>who we are</h1>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.</h4>
+            </div>
+        </div>
+        <!-- heading-->
+        <div class="row main-low-margin text-center">
+            <div class="col-md-12 col-sm-12 text-justify">
+                <h3>lorem ipsum dolor sit amet riopsh</h3>
+                <p>About us
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.</p>
+            </div>
+        </div>
+    </div>
+    <!-- row content-->
+    <div class="col-md-4 col-sm-4 text-center">
+        <img class = 'img-circle' src="asset/img/team/team1.png" alt="">
+        <h3>Perso Name</h3>
+        <h4>Job Profile Goes Here</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec enim sapien. Aliquam erat volutpat.
+                         Quisque eu ante at tortor imperdiet gravida nec sed turpis. Phasellus augue augue. </p>
+
+    </div>
+    <div class="col-md-4 col-sm-4 text-center" data-scrollreveal="enter bottom and move 100px,wait 0.4s">
+        <img class="img-circle" src="assets/img/team/team1.png" alt="">
+        <h3>Person Name</h3>
+               <h4>Job Profile Goes Here</h4>
+                <p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec enim sapien. Aliquam erat volutpat.
+                         Quisque eu ante at tortor imperdiet gravida nec sed turpis. Phasellus augue augue. 
+                </p>
+    </div>
+    <div class="col-md-4 col-sm-4 text-center" data-scrollreveal="enter right and move 100px, wait 0.4s">
+                <img class="img-circle" src="assets/img/team/team1.png">
+                <h3>Person Name</h3>
+               <h4>Job Profile Goes Here</h4>
+                <p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec enim sapien. Aliquam erat volutpat.
+                         Quisque eu ante at tortor imperdiet gravida nec sed turpis. Phasellus augue augue. 
+                </p>
+            </div>
+</div>
+</div>
