0abcccd658a9a9f7af0c273c4ae8af56830c6f6a
YayaIniee <yayainiee@gmail.com>
2022-04-19 00:17:18 +0700

    menambahkan file login.php

---
+++ pages/login.php
@@ -0,0 +1,33 @@
+<div class="container-fluid">
+    <div class="container">
+        <div class="span5">
+            <h2>Sing In</h2>
+            <form action="" method="post">
+                <table class="table">
+                    <tr>
+                        <td>Email</td>
+                        <td>:</td>
+                        <td><input type="text" name="email" required class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>:</td>
+                        <td><input type="text" name="password" id="" required class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Confirm Password</td>
+                        <td>:</td>
+                        <td><input type="text" name="password2" id="" required class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td><input type="submit" name="btnLogin" value="Login" class="btn btn-warning"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+
+    //button di klick
+</div>
