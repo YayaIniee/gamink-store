65ad57b9b91d7b75bedf0e5fc98b6aabf572b264
YayaIniee <yayainiee@gmail.com>
2022-04-19 00:35:58 +0700

    menambahkan file register.php

---
+++ pages/register.php
@@ -0,0 +1,35 @@
+<div class="container-fluid">
+    <div class="container">
+        <div class="span5">
+            <h2>Sing Up</h2>
+            <form action="" method="post">
+                <table class="table">
+                    <tr>
+                        <td>Name</td>
+                        <td>:</td>
+                        <td><input type="text" name="nama" id="nama" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>E-mail</td>
+                        <td>:</td>
+                        <td><input type="email" name="email" maxlength="50" id="" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>:</td>
+                        <td><input type="password" name="passowrd" id="password" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td> <td></td> 
+                        <td>
+                            <input type="submit" value="Save" name="btnSubmit" class="btn btn-warning">
+                            <input type="button" value="Cancel" name="btnReset" class="btn btn-warning">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+</div>
+
+//jika sumbit button di klik
